3e1a85019a95421867c367915bf0fc58bbe23f18
Jesús Fernández <fernandez.cuesta@gmail.com>
2015-04-22 18:01:23 +0200

    Set default vagrant user pwd in script

--- script.sh
+++ script.sh
@@ -17,6 +17,7 @@ grubby --update-kernel=ALL --args="console=ttyS0"
 
 # Add user for vagrant
 useradd vagrant -G wheel
+echo -e 'vagrant\nvagrant' | passwd vagrant
 
 # Put dotfiles in place
 mv dotfiles/nanorc/* /usr/share/nano/
